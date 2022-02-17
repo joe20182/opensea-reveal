f7b6c46d28cea6c0fdeff942956cd07c02176182
joe20182 <joe790924@gmail.com>
2022-02-17 22:20:56 +0800

    code split

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-function handleCheck() {
+const ipfsGateways = ["https://gateway.ipfs.io/ipfs/"];
+
+async function handleCheck() {
   // const link = document.querySelector(".link-input").value;
   const link =
     "https://opensea.io/assets/0xed5af388653567af2f388e6224dc7c4b3241c544/2388";
@@ -6,7 +8,9 @@ function handleCheck() {
   const tokenId = sLink.pop();
   const contractAddress = sLink.pop();
   // console.log(contractAddress, tokenId);
-  getTokenURI(contractAddress, tokenId);
+  const tokenURI = await getTokenURI(contractAddress, tokenId);
+  const metaData = await getMetaData(tokenURI);
+  renderImage(metaData.image);
 }
 
 function getTokenURI(address, id) {
@@ -19,35 +23,48 @@ function getTokenURI(address, id) {
       type: "function",
     },
   ];
-  // const web3 = new Web3(window.ethereum);
 
+  // const web3 = new Web3(window.ethereum);
   const web3 = new Web3(
     Web3.givenProvider || "http://localhost:8543",
     null,
     {}
   );
 
-  // const web3 = new Web3(new Web3.providers.HttpProvider());
-  // await window.ethereum.enable();
-
   const contract = new web3.eth.Contract(abi, address);
-  // console.log(contract);
-  contract.methods
-    .tokenURI(id)
-    .call()
-    .then((res) => {
-      console.log(res);
-      fetch(res)
-        .then((fetchData) => {
-          return fetchData.json();
-        })
-        .then((result) => {
-          console.log(result.image);
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+  return new Promise((resolve, reject) => {
+    contract.methods
+      .tokenURI(id)
+      .call()
+      .then((res) => {
+        // console.log(res);
+        resolve(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject();
+      });
+  });
+}
+
+function getMetaData(tokenURI) {
+  return new Promise((resolve, reject) => {
+    fetch(tokenURI)
+      .then((fetchData) => {
+        return fetchData.json();
+      })
+      .then((result) => {
+        // console.log(result.image);
+        resolve(result);
+      });
+  });
+}
+
+function renderImage(imageURI) {
+  const img = document.createElement("img");
+  img.src = imageURI;
+  document.querySelector(".image-section").appendChild(img);
 }
 
 handleCheck();
