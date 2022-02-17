15e40eb41c242ac4a2c3a86f0baeb95c55a2ca7d
joe20182 <joe790924@gmail.com>
2022-02-18 00:21:47 +0800

    ipfs supported

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
-const ipfsGateways = ["https://gateway.ipfs.io/ipfs/"];
+const ipfsGateways = [
+  "https://cloudflare-ipfs.com/ipfs/",
+  "https://gateway.ipfs.io/ipfs/",
+];
 
 async function handleCheck() {
-  // const link = document.querySelector(".link-input").value;
-  const link =
-    "https://opensea.io/assets/0xed5af388653567af2f388e6224dc7c4b3241c544/2388";
+  const link = document.querySelector(".link-input").value;
+  // const link =
+  //   "https://opensea.io/assets/0xce141c45619e9adbdbdda5af19b3052ff79d5663/147";
   let sLink = link.split("/");
   const tokenId = sLink.pop();
   const contractAddress = sLink.pop();
@@ -34,37 +37,76 @@ function getTokenURI(address, id) {
   const contract = new web3.eth.Contract(abi, address);
 
   return new Promise((resolve, reject) => {
+    setLoading(true);
+
     contract.methods
       .tokenURI(id)
       .call()
       .then((res) => {
         // console.log(res);
+        setLoading(false);
         resolve(res);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
+        alert("please try it later QAQ");
         reject();
       });
   });
 }
 
 function getMetaData(tokenURI) {
+  let uri = tokenURI;
+  if (tokenURI.startsWith("ipfs://")) {
+    uri = `${ipfsGateways[0]}${tokenURI.replace("ipfs://", "")}`;
+  }
+
   return new Promise((resolve, reject) => {
-    fetch(tokenURI)
+    setLoading(true);
+    fetch(uri)
       .then((fetchData) => {
         return fetchData.json();
       })
       .then((result) => {
         // console.log(result.image);
+        setLoading(false);
         resolve(result);
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("please try it later Q_Q");
+        reject();
       });
   });
 }
 
 function renderImage(imageURI) {
+  resetImage();
+
+  let uri = imageURI;
+  if (imageURI.startsWith("ipfs://")) {
+    uri = `${ipfsGateways[0]}${imageURI.replace("ipfs://", "")}`;
+  }
+
   const img = document.createElement("img");
-  img.src = imageURI;
+  img.src = uri;
   document.querySelector(".image-section").appendChild(img);
 }
 
-handleCheck();
+function resetImage() {
+  if (!document.querySelector(".image-section").firstChild) return;
+  document
+    .querySelector(".image-section")
+    .removeChild(document.querySelector(".image-section").firstChild);
+}
+
+function setLoading(flag) {
+  if (flag) {
+    document.querySelector(".loading").style.display = "flex";
+  } else {
+    document.querySelector(".loading").style.display = "none";
+  }
+}
+
+document.querySelector(".check-btn").addEventListener("click", handleCheck);
